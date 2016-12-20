7e61c495ebaf57b3cce4e2a1b472a58e7e742263
Vladimir Shukhov (work) <shukhov88@gmail.com>
2016-12-20 15:07:58 +0300

    uploading editions of MatrixMaxValuesToDiagonal task

--- chapter2/src/main/java/com/shukhov/chapter2/App.java
+++ chapter2/src/main/java/com/shukhov/chapter2/App.java
@@ -7,13 +7,20 @@ public class App
 {
     public static void main( String[] args ) throws IOException {
 
+        //Создание квадратного уравнения с вводом параметров с консоли
         QuadraticEquation equation = new QuadraticEquation();
+        //Выыод в консоль корней созданного уравнения
         equation.printRoots();
+
+        //Вывод в консоль названия месяца по введенному с консоли номеру месяца
         MonthOfTheYear.printMonth();
-        /*int[][] matrix = MatrixMaxValuesToDiagonal.initMatrix();
-        MatrixMaxValuesToDiagonal.printMatrix(matrix);
-        MatrixMaxValuesToDiagonal.sortMaxToDiagonal(matrix);
-        MatrixMaxValuesToDiagonal.printMatrix(matrix);*/
+
+        //Создание двумерной матрицы с введенной с консоли размерностью (n), которая заполняется рандомными числами от -n до n
+        MatrixMaxValuesToDiagonal matrix = new MatrixMaxValuesToDiagonal();
+        //Сортировка созданной матрицы. Перестановка макс значений матрицы сверху вниз по диагонали.
+        matrix.sortMaxToDiagonal();
+        //Вывод значений матрицы в консоль
+        matrix.printMatrix();
 
     }
 }
--- chapter2/src/main/java/com/shukhov/chapter2/MatrixMaxValuesToDiagonal.java
+++ chapter2/src/main/java/com/shukhov/chapter2/MatrixMaxValuesToDiagonal.java
@@ -8,25 +8,29 @@ import java.util.Random;
 /*
 * Задача № 20, Вариант С (Глава 2).
 * Сортировка матрицы заданной размерности. Большие числа расположить по диагонали матрицы.
-* Эта задача еще не до конца решена!!!
 */
-
 
 public class MatrixMaxValuesToDiagonal {
 
-    public static int[][] initMatrix() {
+    private int [][] matrix;
+
+    public MatrixMaxValuesToDiagonal() {
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int matrixSize = 0;
-        int [][] matrix;
 
         try {
             System.out.println("Введи размерность матрицы:");
             matrixSize = Integer.parseInt(reader.readLine());
-            reader.close();
 
         } catch (IOException e) {
             System.out.println(e);
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException io) {
+                System.out.println(io);
+            }
         }
 
         matrix = new int [matrixSize] [matrixSize];
@@ -37,18 +41,16 @@ public class MatrixMaxValuesToDiagonal {
                 matrix[i][j] = randGen.nextInt(matrixSize*2 + 1) - matrixSize;
             }
         }
-
-        return matrix;
     }
 
-    public static void sortMaxToDiagonal(int[][] matrix) {
+    public void sortMaxToDiagonal() {
 
         for (int k = 0; k < matrix.length; k++) {
 
             for (int i = 0; i < matrix.length; i++) {
                 for (int j = 0; j < matrix.length; j++) {
                     if (matrix[k][k] < matrix[i][j]) {
-                        if (i!=j || ((i==j) && (i <= k))) {
+                        if (i!=j || i > k) {
                             int temp = matrix[k][k];
                             matrix[k][k] = matrix[i][j];
                             matrix[i][j] = temp;
@@ -61,7 +63,7 @@ public class MatrixMaxValuesToDiagonal {
 
     }
 
-    public static void printMatrix(int[][] matrix) {
+    public void printMatrix() {
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix.length; j++) {
